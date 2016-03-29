3bb7756862c5dbb0119e05c3de57faad020c243a
CAD Client <CAD Client>
2016-03-29 12:38:21 +0900

    deleted my_

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,20 +1,20 @@
 # -*- coding: cp949
-my_name = 'jang woo seung'
-my_age = 21
-my_height_cm = 173
+name = 'jang woo seung'
+age = 21
+height_cm = 173
 cm_to_inch = 1.0 / 2.54
-my_height_inch =  my_height_cm * cm_to_inch
+my_height_inch = height_cm * cm_to_inch
 my_weight = 79
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d cm tall." % my_height_cm
+print "Let's talk about %s." % name
+print "He's %d cm tall." % height_cm
 print "He's %d inch tall." % my_height_inch
 print "He's %d kg heavy." % my_weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
-print "If i add %d, %d, and %d I get %d." % (my_age, my_height_cm, my_weight, my_age+my_height_cm+my_weight)
+print "If i add %d, %d, and %d I get %d." % (age, height_cm, my_weight, age + height_cm + my_weight)
