2d194957866d9c0f28f39efce5b8815758603f14
CAD Client <CAD Client>
2016-04-12 09:51:41 +0900

    ex11 studydrill

--- ex11/ex11.py
+++ ex11/ex11.py
@@ -4,5 +4,8 @@ print "How tall are you?",
 height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
+print "What is your grade",
+grade = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print "and your grade is %r" % (grade)
